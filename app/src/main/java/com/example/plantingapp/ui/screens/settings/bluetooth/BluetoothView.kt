d3feb35ae7fc2784c403025b0a5655fda5c73df9
Iascatt <veronika.terekhova.2003@gmail.com>
2023-11-15 19:10:15 +0300

    bluetooth fix

--- app/src/main/java/com/example/plantingapp/ui/screens/settings/bluetooth/BluetoothView.kt
+++ app/src/main/java/com/example/plantingapp/ui/screens/settings/bluetooth/BluetoothView.kt
@@ -5,36 +5,65 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BluetoothView(viewModel: BluetoothViewModel){
     val isConnected = viewModel.isConnected
+    var deviceName by remember { mutableStateOf("") }
     Column(
         modifier = Modifier
             .fillMaxWidth()
             .padding(20.dp),
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.Start
     ) {
-        if (!isConnected.value) {
-            Text("Bluetooth устройство не подключено")
-            Text("Подключите устройство HC-05 в настройках приложения")
-            Button(onClick = { viewModel.connectToHC() }) {
-                Text("Повторить подключение")
+
+        Text(
+            text = "Bluetooth",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+        )
+        Text(
+            text = "Введите название устройства",
+            modifier = Modifier.padding(top = 50.dp)
+        )
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = deviceName,
+            onValueChange = { deviceName = it }
+        )
+        when (!isConnected.value) {
+            true -> {
+                Text("Bluetooth устройство не подключено")
+                Text("Подключите устройство в настройках устройства")
             }
-        } else {
-            Text("Bluetooth устройство подключено")
-            Button(onClick = { viewModel.sendMessageToHC("Channel_1") }) {
-                Text("Channel_1")
+            false -> {
+                Text("Bluetooth устройство подключено")
+                Button(onClick = { viewModel.sendMessageToHC("Channel_1") }) {
+                    Text("Channel_1")
+                }
+                Button(onClick = { viewModel.sendMessageToHC("Channel_2") }) {
+                    Text("Channel_2")
+                }
             }
-            Button(onClick = { viewModel.sendMessageToHC("Channel_2") }) {
-                Text("Channel_2")
-            }
+        }
+        Button(onClick = { viewModel.connectToHC(deviceName) }) {
+            Text("Повторить подключение")
         }
     }
 }
--- app/src/main/java/com/example/plantingapp/ui/screens/settings/bluetooth/BluetoothViewModel.kt
+++ app/src/main/java/com/example/plantingapp/ui/screens/settings/bluetooth/BluetoothViewModel.kt
@@ -7,33 +7,59 @@ import android.util.Log
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.example.plantingapp.management.PermissionsManager
+import java.io.IOException
 import java.util.UUID
+
 
 const val REQUEST_ENABLE_BT = 1
 
 class BluetoothViewModel: ViewModel() {
-    private val MY_UUID: UUID = UUID.fromString("c41387de-80a1-11ee-b962-0242ac120002")
+    private val MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
     var bluetoothAdapter: BluetoothAdapter? = null
     var pm: PermissionsManager? = null
 
     val isConnected = mutableStateOf(false)
-    private val deviceHC05 = connectToHC()
+    private var deviceHC05: BluetoothDevice? = null
 
     fun sendMessageToHC(message: String) {
         if (pm?.checkBtPermission() == true) {
             val socket: BluetoothSocket? =
-                deviceHC05?.createRfcommSocketToServiceRecord(MY_UUID)
-            if (socket != null) {
-                if (socket.isConnected) {
-                    val outputStream = socket.outputStream
-                    outputStream.write(message.toByteArray())
-                    outputStream.flush()
+                deviceHC05?.createInsecureRfcommSocketToServiceRecord(MY_UUID)
+            Log.d("kilo", "Device: ${deviceHC05?.name ?: "-"}")
+            Log.d("kilo", "Socket.isConnected: ${socket?.isConnected ?: "doesn't exist"}")
+            if (socket != null && deviceHC05 != null) {
+                try {
+                    var bluetoothSocket: BluetoothSocket? = deviceHC05!!.javaClass.getMethod(
+                        "createRfcommSocket", Int::class.javaPrimitiveType
+                    ).invoke(deviceHC05, 1) as BluetoothSocket?
+                    Log.d(
+                        "kilo",
+                        "BTSocket.isConnected: ${bluetoothSocket?.isConnected ?: "doesn't exist"}"
+                    )
+                    if (bluetoothSocket != null) {
+                        bluetoothSocket.connect()
+                        Log.d(
+                            "kilo",
+                            "BTSocket.isConnected: ${bluetoothSocket.isConnected ?: "doesn't exist"}"
+                        )
+                        if (bluetoothSocket.isConnected == true) {
+                            Log.d("kilo", "Open output stream")
+                            val outputStream = bluetoothSocket.outputStream
+                            Log.d("kilo", "Writing data...")
+                            outputStream.write(message.toByteArray())
+                            Log.d("kilo", "Wrote data")
+                            outputStream.flush()
+                        }
+                    }
+                } catch (exception: IOException){
+                    socket.close()
+                    Log.e("kilo", "Ошибка при подключении сокета: ${exception}")
                 }
             }
         }
     }
 
-     fun connectToHC(): BluetoothDevice? {
+     fun connectToHC(name: String) {
          Log.d("kilo", "bluetoothAdapter?.isEnabled: ${bluetoothAdapter?.isEnabled}")
          Log.d("kilo", "pm?.checkBtPermission() : ${pm?.checkBtPermission() }")
         if (bluetoothAdapter?.isEnabled == true && pm?.checkBtPermission() == true) {
@@ -42,14 +68,13 @@ class BluetoothViewModel: ViewModel() {
             if (pairedDevices != null) {
                 for (device in pairedDevices) {
                     Log.d("kilo", device.name)
-                    if (device.name == "HC-05") {
+                    if (device.name == name) {
                         isConnected.value = true
-                        return device
+                        deviceHC05 = device
+                        break
                     }
                 }
             }
         }
-        isConnected.value = false
-        return null
     }
 }
